71953ba7202c0fb4c84d07331b0cff61fd47603a
YeYaBo <644631077@qq.com>
2019-04-11 21:37:12 +0800

    单文件夹图片转PDF成功

---
+++ PictureToPDF/MainForm.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace PictureToPDF
+{
+    partial class 图片转PDF工具
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.MessageText = new System.Windows.Forms.Label();
+            this.PdfName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(25, 10);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(207, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "选择转换图片的目录";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(22, 105);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "转换图片的数目";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(160, 104);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(70, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(233, 105);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "页";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(25, 186);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 26);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "图片转换";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // MessageText
+            // 
+            this.MessageText.AutoSize = true;
+            this.MessageText.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.MessageText.Location = new System.Drawing.Point(22, 61);
+            this.MessageText.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.MessageText.Name = "MessageText";
+            this.MessageText.Size = new System.Drawing.Size(69, 19);
+            this.MessageText.TabIndex = 5;
+            this.MessageText.Text = "label3";
+            // 
+            // PdfName
+            // 
+            this.PdfName.Location = new System.Drawing.Point(172, 145);
+            this.PdfName.Name = "PdfName";
+            this.PdfName.Size = new System.Drawing.Size(200, 21);
+            this.PdfName.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(22, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 19);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "转换的PDF名字:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(25, 234);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(287, 88);
+            this.listBox1.TabIndex = 8;
+            // 
+            // 图片转PDF工具
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 366);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.PdfName);
+            this.Controls.Add(this.MessageText);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "图片转PDF工具";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label MessageText;
+        private System.Windows.Forms.TextBox PdfName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ PictureToPDF/MainForm.cs
@@ -0,0 +1,141 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Image = System.Drawing.Image;
+
+namespace PictureToPDF
+{
+    public partial class 图片转PDF工具 : Form
+    {
+        private List<string> ImagePaths = new List<string>();    //引入图片的地址
+        private List<Image> images = new List<Image>();
+        const int WWidth = 600;
+        const int HHeight = 800;
+        string PdfFileName = "";
+        string Fileimport;   
+        public 图片转PDF工具()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Fileimport = folderBrowserDialog1.SelectedPath;
+            }
+            MessageText.Text = Fileimport;
+            GetImagePath(MessageText.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (PdfName.Text != "")
+            {
+                PdfFileName = Fileimport + "//" + PdfName.Text;
+            }
+            else
+            {
+                PdfFileName = "C://Users//yyb//Desktop//图片转PDF.pdf";
+            }
+            ImageConvertToPdf(ImagePaths);
+        }
+
+        public void ImageConvertToPdf(List<string> sourcepath)
+        {
+            ChangeTheImageToStandard(ImagePaths);
+            Document document = new Document();
+            document.SetPageSize(new iTextSharp.text.Rectangle(WWidth + 72f, HHeight + 72f));
+            PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(PdfFileName, FileMode.OpenOrCreate, FileAccess.Write));
+            document.Open();
+            iTextSharp.text.Image image;
+            for (int i = 0; i < images.Count; i++)
+            {
+                image = iTextSharp.text.Image.GetInstance(images[i], ImageFormat.Jpeg);
+                document.NewPage();
+                document.Add(image);
+            }
+            if (document != null && document.IsOpen())
+            {
+                document.Close();
+            }
+            if (writer != null)
+            {
+                writer.Close();
+            }
+        }
+        //将所有的图片转换成标准图片
+        public void ChangeTheImageToStandard(List<string> ImageName)
+        {
+            for(int i = 0; i < ImageName.Count; i++)
+            {
+                Bitmap bitmap = new Bitmap(ImageName[i]);
+                Bitmap bmImage = new Bitmap(WWidth, HHeight);
+                Graphics graphics = Graphics.FromImage(bmImage);
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.DrawImage(bitmap,new System.Drawing.Rectangle(0, 0,bmImage.Width, bmImage.Height), new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height), GraphicsUnit.Pixel);
+                graphics.Dispose();
+                images.Add(bmImage);
+            }
+        }
+        public void GetImagePath(string Fileimport)
+        {
+            List<string> files = new List<string>(Directory.GetFiles(Fileimport, "*.*", SearchOption.TopDirectoryOnly).Where(s => s.EndsWith(".jpg") || s.EndsWith(".tif")));
+            files.ForEach(c =>
+            {  
+                string destFile = Path.Combine(new string[] { Fileimport , Path.GetFileName(c) });//destFile目标图片文件名    destPath目标地址
+                ImagePaths.Add(destFile);
+            });
+        }
+             
+        public void CopyFolder(string sourcePath, string destPath)
+        {
+            if (Directory.Exists(sourcePath))
+            {
+                if (!Directory.Exists(destPath))
+                {
+                    //目标目录不存在则创建
+                    try
+                    {
+                        Directory.CreateDirectory(destPath);
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception("创建目标目录失败：" + ex.Message);
+                    }
+                }
+                //获得源文件下所有图片文件
+                List<string> files = new List<string>(Directory.GetFiles(sourcePath, "*.*", SearchOption.TopDirectoryOnly).Where(s => s.EndsWith(".jpg") || s.EndsWith(".tif")));
+                files.ForEach(c =>
+                {
+                    string destFile = Path.Combine(new string[] { destPath, Path.GetFileName(c) });//destFile目标图片文件名    destPath目标地址
+                   
+                });
+                //获得源文件下所有目录文件
+                List<string> folders = new List<string>(Directory.GetDirectories(sourcePath));
+                folders.ForEach(c =>
+                {
+                    string destDir = Path.Combine(new string[] { destPath, Path.GetFileName(c) });
+                    //采用递归的方法实现
+                    CopyFolder(c, destDir);
+                });
+            }
+            else
+            {
+                throw new DirectoryNotFoundException("源目录不存在！");
+            }
+        }
+    }
+}
